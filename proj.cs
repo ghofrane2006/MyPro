4a0255eb46e9f2e1333674f1cf3dcbf5e05f2200
ghofrane777 <ghofrane.azizi@external.forvia.com>
2026-01-14 23:24:58 +0100

    Add user authentication program in proj.cs

---
+++ proj.cs
@@ -0,0 +1,27 @@
+public class program
+{
+    static void Main (string[] args)
+    {
+        string username;
+        string password;
+        int tentatives=3;
+        while(tentatives>0)
+        {
+            Console.WriteLine("Nom d'utilisateur:");
+            username=Console.ReadLine();
+            Console.WriteLine("Mot de passe:");
+            password=Console.ReadLine();
+            if(username=="admin" && password=="admin")
+            {
+                Console.WriteLine("Connexion réussie!");
+                break;
+            }
+            else
+            {
+                tentatives--;
+                Console.WriteLine("Nom d'utilisateur ou mot de passe incorrect. Il vous reste " + tentatives + " tentatives.");
+            }   
+        }
+
+    }
+}
